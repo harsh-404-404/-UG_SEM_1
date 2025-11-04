99f62d58cec01b7807b613d1a91f034333da7134
harsh-404-404 <harshbhalala404@gmail.com>
2025-11-04 21:47:44 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	renamed:    gen_ai_python/conditional -> gen_ai_python/basics/conditional
    	renamed:    gen_ai_python/dictionary.py -> gen_ai_python/basics/dictionary.py
    	renamed:    gen_ai_python/functon_arguments_types.py -> gen_ai_python/basics/functon_arguments_types.py
    	renamed:    gen_ai_python/keyworddss.py -> gen_ai_python/basics/keyworddss.py
    	renamed:    gen_ai_python/list.py -> gen_ai_python/basics/list.py
    	renamed:    gen_ai_python/loop.py -> gen_ai_python/basics/loop.py
    	renamed:    gen_ai_python/loop2.py -> gen_ai_python/basics/loop2.py
    	renamed:    gen_ai_python/mixed_argument.py -> gen_ai_python/basics/mixed_argument.py

--- gen_ai_python/loop.py
+++
@@ -1,5 +0,0 @@
-
-my_sub = input("Enter your subject: ")
-for i in range(len(my_sub)):
-    print(i , "-->" , my_sub[i])
-    
--- gen_ai_python/loop2.py
+++
@@ -1,3 +0,0 @@
-my_list = [97,794,34,23]
-for i in my_list:
-    print(i)
