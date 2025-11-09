e6311bcb27cde6207175dff21142d1d2321ce73b
harsh-404-404 <harshbhalala404@gmail.com>
2025-11-09 21:40:10 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   basic_coding/pset_1/8_sum_best_four.c
    	new file:   basic_coding/pset_1/8_sum_best_four.exe

--- basic_coding/pset_1/8_sum_best_four.c
+++ basic_coding/pset_1/8_sum_best_four.c
@@ -1,29 +1,53 @@
 #include <stdio.h>
 
+void get_ip(int* num,int len);
+void sort(int* num,int len);
+int sum(int* num,int len);
+
 int main(void){
 
-    int num[5];
+    int len = 5;
+    int num[len];
 
-    for(int i = 0; i < 5; ++i){
+    get_ip(num,len);
+    sort(num,len);
+    printf("Sum of top 4 number is :%d",sum(num,len));
+}
 
-        printf("%d.)enter a number: ");
-        scanf("%d",num[i]);
+
+void get_ip(int* num,int len){
+
+    for(int i = 0; i < len; ++i){
+
+        printf("%d.enter a number: ",i+1);
+        scanf("%d",&num[i]);
     }
     
+}
 
-    for(int i = 4; i >= 0; --i){
+void sort(int* num,int len){
+
+    for(int i = len-1; i > 0; --i){
         
-        for(int k = 0; k < 5;++k){
+        for(int k = 0; k < i;++k){
             
-            if(num[k] >= num[k+1]){
+            if(num[k] < num[k+1]){
                 num[k] += num[k+1];
                 num[k+1] = num[k] - num[k+1];
                 num[k] -= num[k+1];
+
             }
         }
-
-        
     }
-    
-    
+
+}
+
+int sum(int* num,int len){
+
+    int sum = 0;
+    for(int i = 0; i < 4; ++i){
+        sum += num[i];
+    }
+    return sum;
+
 }
