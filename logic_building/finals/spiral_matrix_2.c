49ad030fbb8d905d9f8aeb35b08242ef372e863a
harsh-404-404 <harshbhalala404@gmail.com>
2025-12-20 12:36:26 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   logic_building/finals/spiral_matrix_2.c
    	new file:   logic_building/finals/spiral_matrix_2.exe

---
+++ logic_building/finals/spiral_matrix_2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void){
+
+    int m,n;
+    printf("Enter a row of matrix:");
+    scanf("%d",&n);
+    printf("Enter a column matrix:");
+    scanf("%d",&m);
+
+    int matrix[m][n];
+
+    int top = 0,bottom = n-1,left = 0,right = m-1;
+    int digit = 1;
+
+    while(top <= bottom && left <= right){
+        
+        for(int j = left; j <= right;++j){
+            matrix[top][j] = digit++;
+        }top++;
+        for(int i = top; i <= bottom;++i){
+            matrix[i][right] = digit++;
+        }right--;
+
+        if(top > bottom || left > right)break;  
+
+        for(int j = right; j >= left;--j){
+            matrix[bottom][j] = digit++;
+        }bottom--;
+        for(int i = bottom; i >= top;--i){
+            matrix[i][left] = digit++;
+        }left++;
+
+    }
+
+    for(int i = 0; i < n; ++i){
+        for(int j = 0; j < m;++j){
+            printf("\t%d",matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
